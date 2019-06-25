e3d10e8c58211dc24cd865c784ef331f9c93a9fc
lee <logicleeee+github@gmail.com>
2019-06-25 09:44:14 -0700

    inital commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    '@vue/prettier'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none',
+        tabWidth: 2,
+        semi: false,
+        singleQuote: true,
+        arrowParens: 'always'
+      }
+    ],
+    'comma-dangle': 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
